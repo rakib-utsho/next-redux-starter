b8bbe55533bb77554ec88c20a3f29563076b8260
Md. Rakibul Islam <77563111+rakib-utsho@users.noreply.github.com>
2026-01-29 10:09:49 +0600

    update worker function

--- src/app/(authLayout)/auth/signin/page.tsx
+++ src/app/(authLayout)/auth/signin/page.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-const SignInPage = () => {
-	return (
-		<div>
-			SignInPage
-		</div>
-	);
+export default function page() {
+  return (
+	<div>
+	  signin
+	</div>
+  )
 }
-
-export default SignInPage;
